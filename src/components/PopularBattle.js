4cc32bdb65babd210e379b049b99869d921018bb
jan lawrence <56373608+janhawk@users.noreply.github.com>
2020-08-05 00:41:25 +0200

    react_moovice

--- src/components/PopularBattle.js
+++ src/components/PopularBattle.js
@@ -11,31 +11,27 @@ class PopularBattle extends React.Component {
       currentPage: 1,
     };
   }
+ 
   componentDidMount() {
     const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=87be3fa1b183c85e6672f543408b2516';
-    
     fetch(url)
     .then(res => res.json())
     .then(data => {
 
       const movies = data.results;
-      const key = data.results;
-      const currentPage = data.results;
       console.log(data);
       // code
       this.setState({
-        movies: data.results,
-        key: data.results,
-        currentPage: data.results,
+        movies: data.results
       })
     });
   }
-  nextPage = (id) => {
+
+  onclick = () => {
     this.setState({
       currentPage: this.state.currentPage + 1
     })
   }
-
 
   render() {
     if (this.state.movies.length ===0) {
@@ -47,24 +43,28 @@ class PopularBattle extends React.Component {
     }
     return (
       <div>
-        <h1>popular battle</h1>
-        <div>
-        {this.state.movies.map((movie) => {
-          return (
-            <Card
-            
-            {...movie}
-            // onclick= {this.onclickMovie} 
-              // title = {movie.title}
-              // poster_path = {movie.poster_path}
-            /> 
-            
-          );
-        })}
-       
-        </div>
+          <h1>PopularBattle</h1>
+            <div className="row">
+              
+                {this.state.movies.slice(0 + (this.state.currentPage - 1) * 2, 2 + (this.state.currentPage - 1) * 2).map((movie) => {
+                  return (
+                    <Card 
+                  
+                      {...movie}
+                  
+                      description={movie.overview}
+                      img={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                      onclick={() => {
+                    
+                      this.onclick(movie.id)
+                  }}/>
+                        )
+                  })}
+          
+            </div>
       </div>
     );
   }
 }
+
 export default PopularBattle;
